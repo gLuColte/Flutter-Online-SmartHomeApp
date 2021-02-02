417171372674dd184791dd518b6edfa4fb870d5f
garyJune <gary.lu.unsw@gmail.com>
2021-02-02 20:16:20 +1100

    Drawer and tab updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:custom_switch/custom_switch.dart';
+import 'package:flutter_knob/flutter_knob.dart';
+import 'package:dashed_container/dashed_container.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -110,6 +112,10 @@ class _roomTemperatureState extends State<roomTemperature> {
   //Variables
   int speed = 1;
   bool onOffToggle = false;
+  double _value = 16.0;
+  void _setValue(double value) => setState(() => _value = value);
+  static const double minValue = 16;
+  static const double maxValue = 30;
 
   @override
   Widget build(BuildContext context) {
@@ -118,78 +124,143 @@ class _roomTemperatureState extends State<roomTemperature> {
         // TODO: Knob Controller
         Expanded(
           flex: 4,
-          child: Container(
-            color: Colors.green,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 30,),
+              Expanded(
+                flex: 4,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      width: 250,
+                      height: 250,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: DashedContainer(
+                          dashColor: Theme.of(context).accentColor,
+                          boxShape: BoxShape.circle,
+                          dashedLength: 3.0,
+                          blankLength: 3.0,
+                          strokeWidth: 3.0,
+                          child: Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Knob(
+                              value: _value,
+                              color: Color(0xFF494949),
+                              onChanged: _setValue,
+                              min: minValue,
+                              max: maxValue,
+                              size: 170,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      child: Icon(FontAwesomeIcons.plus, size: 15, color: Theme.of(context).accentColor,),
+                      top: 115,
+                      left: 0.0,
+                    ),
+                    Positioned(
+                      child: Icon(FontAwesomeIcons.minus, size: 15, color: Theme.of(context).accentColor,),
+                      top: 115,
+                      right: 0.0,
+                    ),
+
+                    Container(
+                      width: 120,
+                      height: 120,
+                      decoration: new BoxDecoration(
+                        color: Colors.black,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Center(child: Text('${_value.toStringAsFixed(0)} C', style: TextStyle(color: Colors.white, fontSize: 30),)),
+                    )
+
+
+                  ]
+                ),
+              ),
+            ],
           ),
         ),
-        // TODO: Wind Speed Controler
         Expanded(
           flex: 2,
           child: Container(
-              child: Container(
-            width: 300,
-            child: Column(
-              children: <Widget>[
-                SizedBox(height: 20,),
-                Center(child: Text('Wind Speed', style: TextStyle(fontWeight: FontWeight.bold),)),
-                Expanded(
-                  child: SizedBox(
-                    width: 250,
-                    child: SliderTheme(
-                      data: SliderTheme.of(context).copyWith(
-                          inactiveTrackColor: Color(0XFF8D8E98),
-                          activeTrackColor: Theme.of(context).accentColor,
-                          thumbColor: Theme.of(context).accentColor,
-                          overlayColor: Theme.of(context).accentColor,
-                          thumbShape:
-                              RoundSliderThumbShape(enabledThumbRadius: 6.0),
-                          overlayShape:
-                              RoundSliderOverlayShape(overlayRadius: 8.0)),
-                      child: Slider(
-                        value: speed.toDouble(),
-                        min: 1.0,
-                        max: 5.0,
-                        onChanged: (double newValue) {
-                          setState(() {
-                            speed = newValue.round();
-                          });
-                        },
+            child: Container(
+              width: 300,
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Center(
+                    child: Text(
+                      'Wind Speed',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      width: 250,
+                      child: SliderTheme(
+                        data: SliderTheme.of(context).copyWith(
+                            inactiveTrackColor: Color(0XFF8D8E98),
+                            activeTrackColor: Theme.of(context).accentColor,
+                            thumbColor: Theme.of(context).accentColor,
+                            overlayColor: Theme.of(context).accentColor,
+                            thumbShape:
+                                RoundSliderThumbShape(enabledThumbRadius: 6.0),
+                            overlayShape:
+                                RoundSliderOverlayShape(overlayRadius: 8.0)),
+                        child: Slider(
+                          value: speed.toDouble(),
+                          min: 1.0,
+                          max: 5.0,
+                          onChanged: (double newValue) {
+                            setState(() {
+                              speed = newValue.round();
+                            });
+                          },
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'Low',
-                        style: TextStyle(
-                          color: Colors.grey,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          'Low',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Medium',
-                        style: TextStyle(
-                          color: Colors.grey,
+                        Text(
+                          'Medium',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
                         ),
-                      ),
-                      Text(
-                        'High',
-                        style: TextStyle(
-                          color: Colors.grey,
-                        ),
-                      )
-                    ],
+                        Text(
+                          'High',
+                          style: TextStyle(
+                            color: Colors.grey,
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
-                SizedBox(height: 20,),
-              ],
+                  SizedBox(
+                    height: 30,
+                  ),
+                ],
+              ),
             ),
-          )),
+          ),
         ),
-        // TODO: On Off button
         Expanded(
           flex: 1,
           child: Column(
@@ -198,7 +269,7 @@ class _roomTemperatureState extends State<roomTemperature> {
               CustomSwitch(
                 activeColor: Theme.of(context).accentColor,
                 value: onOffToggle,
-                onChanged: (value){
+                onChanged: (value) {
                   print("VALUE : $value");
                   setState(() {
                     onOffToggle = value;
@@ -211,7 +282,6 @@ class _roomTemperatureState extends State<roomTemperature> {
       ],
     );
   }
-
 }
 
 // Build Bottom Bar Items
@@ -276,20 +346,21 @@ class endDrawer extends StatelessWidget {
           children: <Widget>[
             DrawerHeader(
               child: Center(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  CircleAvatar(
-                    radius: 50.0,
-                    backgroundImage:
-                        NetworkImage('http://i.imgur.com/zL4Krbz.jpg'),
-                  ),
-                  Text(
-                    'Jonathan',
-                    style: TextStyle(fontSize: 20),
-                  )
-                ],
-              )),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 50.0,
+                      backgroundImage:
+                          NetworkImage('http://i.imgur.com/zL4Krbz.jpg'),
+                    ),
+                    Text(
+                      'Jonathan',
+                      style: TextStyle(fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
             ),
             buildTile(context: context, icon: Icons.input, text: 'Welcome'),
             buildTile(
